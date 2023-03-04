e1f904e067937b9e41d3a4cc0856845615a2bab5
Moein Verkiani <108926216+moligarch@users.noreply.github.com>
2023-03-04 20:51:04 +0330

    Update hw.py

--- Assignments/401422193/HW1/hw.py
+++ Assignments/401422193/HW1/hw.py
@@ -6,16 +6,15 @@ def multiplyxy(x,y):
     else:
         m = max(len(str(x)),len(str(y)))
         m2 = m // 2
-
         a = x // 10**(m2)
         b = x % 10**(m2)
         c = y // 10**(m2)
         d = y % 10**(m2)
-#we could use return=ac+ad+bc+bd as well as below,it has 4 multiplication but we could reduce it to 3 by:
+        #we could use return=ac+ad+bc+bd as well as below,it has 4 multiplication but we could reduce it to 3 by:
+        
         z0 = multiplyxy(b,d)
         z1 = multiplyxy((a+b),(c+d))
         z2 = multiplyxy(a,c)
-
         return (z2 * 10**(2*m2)) + ((z1 - z2 - z0) * 10**(m2)) + (z0)
 
 print("What do you want to multiply? (Write 2 Integer Number in the same line)")
