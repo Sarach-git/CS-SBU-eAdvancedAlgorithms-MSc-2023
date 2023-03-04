1a57d084632d949733badf912472c890737dd339
Moein Verkiani <108926216+moligarch@users.noreply.github.com>
2023-03-04 20:49:59 +0330

    Update hw.py
    add comment

--- Assignments/401422193/HW1/hw.py
+++ Assignments/401422193/HW1/hw.py
@@ -1,4 +1,6 @@
+#Define Optimized Multiplication Function
 def multiplyxy(x,y):
+    #Simplification of computing + define zero state
     if len(str(x)) == 1 or len(str(y)) == 1:
         return x*y
     else:
@@ -9,13 +11,13 @@ def multiplyxy(x,y):
         b = x % 10**(m2)
         c = y // 10**(m2)
         d = y % 10**(m2)
-
+#we could use return=ac+ad+bc+bd as well as below,it has 4 multiplication but we could reduce it to 3 by:
         z0 = multiplyxy(b,d)
         z1 = multiplyxy((a+b),(c+d))
         z2 = multiplyxy(a,c)
 
         return (z2 * 10**(2*m2)) + ((z1 - z2 - z0) * 10**(m2)) + (z0)
 
-print("What do you want to multiply? (Write 2 Whole/Integer Number)")
+print("What do you want to multiply? (Write 2 Integer Number in the same line)")
 x1,x2=input("").split()
 print(multiplyxy(int(x1),int(x2)))
